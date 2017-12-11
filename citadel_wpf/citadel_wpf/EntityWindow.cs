6884e24575e0c578b5e628b580db45eaba9c7ec5
ryanmahoney96 <ryan.mahoney96@gmail.com>
2017-12-10 19:14:20 -0600

    Event Relationship Update Merge

--- citadel_wpf/citadel_wpf/EntityWindow.cs
+++ citadel_wpf/citadel_wpf/EntityWindow.cs
@@ -30,7 +30,6 @@ namespace citadel_wpf
             SetDecorations(this);
             Uri iconUri = new Uri("../../citadel_logo.png", UriKind.RelativeOrAbsolute);
             Icon = BitmapFrame.Create(iconUri);
-            
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
 
